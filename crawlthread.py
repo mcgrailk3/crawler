138d00ad314ef9c48468074e23a93a65abe00c71
Kevin McGrail <mcgrailk3@udayton.edu>
2021-09-26 11:02:04 -0400

    starting optimization

--- crawlthread.py
+++ crawlthread.py
@@ -107,24 +107,31 @@ class CrawlThread (threading.Thread):
             responsecode = response[0].split(" ")
             # print(f"\tVerifying header... status code {responsecode[1]} {self.threadID}")
             #print("      + Parsing page... ", end='')
-            self.shared.lock.acquire()
-            if responsecode[1][0] == "2":
-                self.shared.responses[0] += 1
-            elif responsecode[1][0] == "3":
-                self.shared.responses[1] += 1
-            elif responsecode[1][0] == "4":
-                self.shared.responses[2] += 1
-            elif responsecode[1][0] == "5":
-                self.shared.responses[3] += 1
-            else:
-                self.shared.responses[4] += 1
-            self.shared.lock.release()
             
-            if responsecode[1][0] == "2" or responsecode[1][0] == "3":
+            if responsecode[1][0] == "2":
                 self.shared.lock.acquire()
+                self.shared.responses[0] += 1
                 self.shared.crawled += 1
                 self.shared.lock.release()
+            elif responsecode[1][0] == "3":
+                self.shared.lock.acquire()
+                self.shared.responses[1] += 1
+                self.shared.crawled += 1
+                self.shared.lock.release()
+            elif responsecode[1][0] == "4":
+                self.shared.lock.acquire()
+                self.shared.responses[2] += 1
+                self.shared.lock.release()
+                continue
+            elif responsecode[1][0] == "5":
+                self.shared.lock.acquire()
+                self.shared.responses[3] += 1
+                self.shared.lock.release()
+                continue
             else:
+                self.shared.lock.acquire()
+                self.shared.responses[4] += 1
+                self.shared.lock.release()
                 continue
 
             links = data.count('href')
